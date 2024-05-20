11efde8f7c5809b1b05d7ba443f2904249c043ff
MbuviM <mwendembuvi10@gmail.com>
2024-05-20 21:31:39 +0300

    Add classes that combine the serializer, model and view.

--- backend/therapists/views.py
+++ backend/therapists/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
-
-# Create your views here.
-from rest_framework import generics
+# therapists/views.py
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Therapist
 from .serializer import TherapistSerializer
 
+class TherapistList(APIView):
+    def get(self, request):
+        therapists = Therapist.objects.all()
+        serializer = TherapistSerializer(therapists, many=True)
+        return Response(serializer.data)
 
-class SnippetList(generics.ListCreateAPIView):
-    queryset = Therapist.objects.all()
-    serializer_class = TherapistSerializer
-
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Therapist.objects.all()
-    serializer_class =  TherapistSerializer
+class TherapistDetail(APIView):
+    def get(self, request, pk):
+        therapist = Therapist.objects.get(pk=pk)
+        serializer = TherapistSerializer(therapist)
+        return Response(serializer.data)
