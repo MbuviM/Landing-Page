eaf6371cec901eacabf47ec25cdd7b7f484e734f
MbuviM <mwendembuvi10@gmail.com>
2024-05-20 17:01:28 +0300

    Add code for combining URLs, Model and serializer.

--- backend/therapists/views.py
+++ backend/therapists/views.py
@@ -2,15 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import generics
-from .models import Snippet
-from .serializer import SnippetSerializer
+from .models import Therapist
+from .serializer import TherapistSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-
+    queryset = Therapist.objects.all()
+    serializer_class = TherapistSerializer
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
+    queryset = Therapist.objects.all()
+    serializer_class =  TherapistSerializer
