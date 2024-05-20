1ff44388cf4a74fe0eb4a3526d8188597a86226e
MbuviM <mwendembuvi10@gmail.com>
2024-05-20 22:18:12 +0300

    Add decoraters for listing all users and adding users

--- backend/therapists/views.py
+++ backend/therapists/views.py
@@ -3,6 +3,7 @@ from .serializer import TherapistSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+# Create your views here.
 @api_view(['GET'])
 def index(request):
    urls = {
@@ -13,3 +14,19 @@ def index(request):
         'Edit post':'edit-post/<uuid:id>',
     }
    return Response(urls)
+
+# List all users
+@api_view(['GET'])
+def listUsers(request):
+    posts = Therapist.objects.all()
+    list = TherapistSerializer(posts,many=True)
+    return Response(list.data)
+
+# Add a user
+@api_view(['POST'])
+def addUsers(request):
+    serializer = TherapistSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
