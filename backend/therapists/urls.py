0014833253abd66143355e911d0da82bbfa34a87
MbuviM <mwendembuvi10@gmail.com>
2024-05-20 22:17:14 +0300

    Add urls for listing all users and adding users

--- backend/therapists/urls.py
+++ backend/therapists/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('user/',views.listUsers,name='posts'),
+    path('user/create/',views.addUsers,name='create-post'),
 ]
