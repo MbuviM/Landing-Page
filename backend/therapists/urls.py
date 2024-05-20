cac0b5cfe6a90bcb59981edbbbca8091f0247f8c
MbuviM <mwendembuvi10@gmail.com>
2024-05-20 21:47:17 +0300

    Change the url.

--- backend/therapists/urls.py
+++ backend/therapists/urls.py
@@ -1,7 +1,6 @@
+from . import views
 from django.urls import path
-from .views import TherapistList, TherapistDetail
 
 urlpatterns = [
-    path('therapists/', TherapistList.as_view()),
-    path('therapists/<int:pk>/', TherapistDetail.as_view()),
+    path('',views.index,name='index'),
 ]
