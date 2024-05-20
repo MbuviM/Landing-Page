8b1c94ce66bc901b3f5fc60ef103148d41bc9e40
MbuviM <mwendembuvi10@gmail.com>
2024-05-20 22:16:34 +0300

    Add created column

--- backend/therapists/models.py
+++ backend/therapists/models.py
@@ -1,10 +1,4 @@
 from django.db import models
-from pygments.lexers import get_all_lexers
-from pygments.styles import get_all_styles
-
-LEXERS = [item for item in get_all_lexers() if item[1]]
-LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
-STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
 # Create your models here.
 class Therapist(models.Model):
@@ -23,10 +17,8 @@ class Therapist(models.Model):
     monthly_slots = models.IntegerField(default=0)
     monthly_fee = models.IntegerField(default=0)
     accepts_queer_clients = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=True)  # Adding the created field
+    created = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        ordering = ["created"]
         
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
