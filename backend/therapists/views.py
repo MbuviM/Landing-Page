42f29c8f5564ebd1dc1358fc6cac9f3b405bdee7
MbuviM <mwendembuvi10@gmail.com>
2024-05-21 15:32:03 +0300

    Modify code to include error handling.

--- backend/therapists/views.py
+++ backend/therapists/views.py
@@ -2,25 +2,26 @@ from .models import Therapist
 from .serializer import TherapistSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 @api_view(['GET'])
 def index(request):
-   urls = {
-        'All Posts':'posts',
-        'View single post':'post/<uuid:id>',
-        'Create post':'create-post',
-        'Delete post':'delete-post/<uuid:id>',
-        'Edit post':'edit-post/<uuid:id>',
+    urls = {
+        'All Posts': 'posts',
+        'View single post': 'post/<uuid:id>',
+        'Create post': 'create-post',
+        'Delete post': 'delete-post/<uuid:id>',
+        'Edit post': 'edit-post/<uuid:id>',
     }
-   return Response(urls)
+    return Response(urls)
 
 # List all users
 @api_view(['GET'])
 def listUsers(request):
-    posts = Therapist.objects.all()
-    list = TherapistSerializer(posts,many=True)
-    return Response(list.data)
+    therapists = Therapist.objects.all()
+    serializer = TherapistSerializer(therapists, many=True)
+    return Response(serializer.data)
 
 # Add a user
 @api_view(['POST'])
@@ -28,5 +29,6 @@ def addUsers(request):
     serializer = TherapistSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-
-    return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
