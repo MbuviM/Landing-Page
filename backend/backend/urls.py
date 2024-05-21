88d6472ad1a8eaf69cbdf2c9003b596b5d28bd1c
MbuviM <mwendembuvi10@gmail.com>
2024-05-21 22:24:16 +0300

    Add urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('therapists/', include('therapists.urls'))
+    path('therapists/', include('therapists.urls')),
 ]
