44fcec8f7717556c3b299889cd7e802408de6014
MbuviM <mwendembuvi10@gmail.com>
2024-05-22 13:11:19 +0300

    Add paths for all the HTML templates.

--- backend/therapists/urls.py
+++ backend/therapists/urls.py
@@ -2,7 +2,11 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('',views.index,name='index'),
     path('search/', views.search_therapists, name='search-therapists'),
     path('user/create/',views.addUsers,name='create-post'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('find-a-therapist/', views.find_a_therapist, name='find_a_therapist'),
+    path('therapists/', views.therapist_registration, name='therapists'),
 ]
+
