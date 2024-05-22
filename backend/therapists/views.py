e737ee665a36b88b36b05f78a4d0a9d23d26d587
MbuviM <mwendembuvi10@gmail.com>
2024-05-22 13:12:03 +0300

    Add view functions for all paths.

--- backend/therapists/views.py
+++ backend/therapists/views.py
@@ -3,6 +3,21 @@ from .serializer import TherapistSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def find_a_therapist(request):
+    return render(request, 'find-a-therapist.html')
+
+def therapist_registration(request):
+    return render(request, 'therapists.html')
 
 # Create your views here.
 @api_view(['GET'])
@@ -15,6 +30,22 @@ def index(request):
         'Edit post': 'edit-post/<uuid:id>',
     }
     return Response(urls)
+
+def index(request):
+    location = request.GET.get('location')
+    type_of_therapy = request.GET.get('type_of_therapy')
+
+    therapists = Therapist.objects.all()
+    if location:
+        therapists = therapists.filter(location__icontains=location)
+    if type_of_therapy:
+        therapists = therapists.filter(type_of_therapy__icontains=type_of_therapy)
+
+    context = {
+        "therapists": therapists,
+    }
+    return render(request, "find-a-therapist.html", context)
+
 
 # Get a therapist
 @api_view(['GET'])
