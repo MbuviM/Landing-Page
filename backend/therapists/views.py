9d624a9ed5b7fdd32ecb70098a4a8d07ca5c4572
MbuviM <mwendembuvi10@gmail.com>
2024-05-22 14:59:48 +0300

    Add function to display data when search button is clicked in find a therapist page.

--- backend/therapists/views.py
+++ backend/therapists/views.py
@@ -5,7 +5,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
+from .forms import SearchForm, TherapistForm
 
 def home(request):
     return render(request, 'index.html')
@@ -31,20 +32,26 @@ def index(request):
     }
     return Response(urls)
 
-def index(request):
-    location = request.GET.get('location')
-    type_of_therapy = request.GET.get('type_of_therapy')
+# Search for therapists
+def search_therapists(request):
+    if request.method == 'GET':
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            location = form.cleaned_data.get('location')
+            therapy_type = form.cleaned_data.get('therapy_type')
 
-    therapists = Therapist.objects.all()
-    if location:
-        therapists = therapists.filter(location__icontains=location)
-    if type_of_therapy:
-        therapists = therapists.filter(type_of_therapy__icontains=type_of_therapy)
+            # Perform search only if both location and therapy type are provided
+            if location and therapy_type:
+                therapists = Therapist.objects.filter(location=location, type_of_therapy=therapy_type)
+            else:
+                therapists = []  # Return an empty list if any field is empty
+        else:
+            therapists = []  # Return an empty list if form is not valid
+    else:
+        form = SearchForm()
+        therapists = []  # Return an empty list if request method is not GET
 
-    context = {
-        "therapists": therapists,
-    }
-    return render(request, "find-a-therapist.html", context)
+    return render(request, 'search_page.html', {'form': form, 'therapists': therapists})
 
 
 # Get a therapist
@@ -72,3 +79,29 @@ def addUsers(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def find_a_therapist(request):
+    if request.method == 'GET':
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            location = form.cleaned_data.get('location')
+            therapy_type = form.cleaned_data.get('type_of_therapy')
+            therapists = Therapist.objects.all()
+            if location:
+                therapists = therapists.filter(location__icontains=location)
+            if therapy_type:
+                therapists = therapists.filter(type_of_therapy__icontains=therapy_type)
+            return render(request, 'find-a-therapist.html', {'form': form, 'therapists': therapists})
+    else:
+        form = SearchForm()
+    return render(request, 'find-a-therapist.html', {'form': form})
+
+def therapist_registration(request):
+    if request.method == 'POST':
+        form = TherapistForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = TherapistForm()
+    return render(request, 'therapists.html', {'form': form})
