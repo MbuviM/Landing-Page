40c81db2b76a9ebebf6417a2b06045cf25671575
MbuviM <mwendembuvi10@gmail.com>
2024-05-31 16:09:37 +0300

    Add TherapistAdmin Model

--- backend/therapists/admin.py
+++ backend/therapists/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Therapist
+from .models import Therapist, Location, Therapy
 
-# Display the Therapist model in the admin panel
+# Therapist Admin Model
 class TherapistAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email', 'phone', 'address', 'location')
-
+    list_display = ('first_name', 'last_name', 'profile_picture','location')
+# Display the Therapist model in the admin panel
 admin.site.register(Therapist, TherapistAdmin)
-
+admin.site.register(Location)
+admin.site.register(Therapy)
