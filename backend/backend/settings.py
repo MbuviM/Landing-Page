eeadc61a6762cbd306b7408721a5fec01c62d0c4
MbuviM <mwendembuvi10@gmail.com>
2024-06-03 12:21:31 +0300

    Add sessions settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -144,3 +144,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Sessions settings
+SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
