70e7cecb15699775440292a9efb6e099ed99a906
MbuviM <mwendembuvi10@gmail.com>
2024-06-03 23:47:03 +0300

    Add form to signup view

--- backend/therapists/views.py
+++ backend/therapists/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.views.decorators.csrf import csrf_protect
 from rest_framework.decorators import api_view
@@ -10,22 +9,38 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
-from .models import Therapist
-from .serializer import TherapistSerializer
+from .models import Therapist, User
+from .serializer import TherapistSerializer, UserSerializer
+from .forms import CreateUserForm, LoginForm
+from django.contrib import messages
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'User created successfully')
             return redirect('login')
+        else:
+            messages.error(request, 'Please correct the errors below.')
     else:
-        form = UserCreationForm()
+        form = CreateUserForm()
 
     context = {'form': form}
     return render(request, 'signup.html', context)
 
 def login(request):
+    form = LoginForm(request.POST)
+    msg = 'None'
+    if form.is_valid():
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return redirect('signup')
     return render(request, 'login.html')
 
 def home(request):
@@ -88,30 +103,6 @@ def therapist_register(request):
         return redirect('success')
 
     return render(request, 'therapists.html')
-
-class UserRegistrationView(APIView):
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-User = get_user_model()
-class UserLoginView(ObtainAuthToken):
-    def post(self, request, *args, **kwargs):
-        username = request.data.get('username')
-        password = request.data.get('password')
-
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            token, created = Token.objects.get_or_create(user=user)
-            if not created:
-                token.delete()
-                token = Token.objects.create(user=user)
-            return Response({'token': token.key, 'username': user.username, 'role': user.role})
-        return Response({'message': 'Invalid username or password'}, status=status.HTTP_401_UNAUTHORIZED)
 
 class UserLogoutView(APIView):
     permission_classes = [IsAuthenticated]
